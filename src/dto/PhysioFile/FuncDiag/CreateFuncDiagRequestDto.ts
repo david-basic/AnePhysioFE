f9fcbda4f57d86e890584b019ee39f62e3411d26
David Bašić <d.basic97.db@gmail.com>
2024-01-20 17:10:41 +0100

    FD modal feat. created, needs debugging

---
+++ src/dto/PhysioFile/FuncDiag/CreateFuncDiagRequestDto.ts
@@ -0,0 +1,4 @@
+export interface CreateFuncDiagRequestDto {
+    physioFileId: string;
+    description: string;
+}
---
+++ src/dto/PhysioFile/FuncDiag/DeleteFunctionalDiagnosisRequestDto.ts
@@ -0,0 +1,4 @@
+export interface DeleteFunctionalDiagnosisRequestDto {
+	fdId: string;
+	physioFileId: string;
+}
